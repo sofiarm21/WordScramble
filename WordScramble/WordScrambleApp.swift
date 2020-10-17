24fd832f0ec1d3f9b6c4087e73b7d2630de7df93
Sofia Rodriguez <sofiaromorales@gmail.com>
2020-10-17 16:43:16 -0400

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Sofia Rodriguez Morales on 10/17/20.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
